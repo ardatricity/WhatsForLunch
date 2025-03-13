b61f073fa26a8ab4701cd167f86545d9d6d3bd1f
ardatricity <ardatricity@gmail.com>
2025-03-13 12:29:26 +0300

    Add base URL to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WhatsForLunch/', // Proje adınız neyse buraya onu yazın
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
